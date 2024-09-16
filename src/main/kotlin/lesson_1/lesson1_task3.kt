21487d7316cd88843f272b1a32bc07d5657439dd
artem <artemmrt@gmail.com>
2024-09-16 21:22:03 +0400

    KS-1-3 convert minute Int to String

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -4,14 +4,14 @@ fun main() {
 
     val year: Int = 1961
     var hour: Int = 9
-    var minute: Int = 7
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
 
     hour = 10
-    minute = 55
+    minute = "55"
 
     print(hour)
     print(":")
