3c794ad542a532f4013e621529e91f6a78f99b5c
artem <artemmrt@gmail.com>
2024-09-17 10:58:21 +0400

    KS-2-1: i did lesson 2-1

---
+++ src/main/kotlin/lesson_2/lesson2_task_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val scoreStudent1 = 3
+    val scoreStudent2 = 4
+    val scoreStudent3 = 3
+    val scoreStudent4 = 5
+
+    val averageScore = (scoreStudent1.toDouble() + scoreStudent2.toDouble() + scoreStudent3.toDouble() + scoreStudent4.toDouble()) / 4
+
+    println(averageScore)
+
+}
