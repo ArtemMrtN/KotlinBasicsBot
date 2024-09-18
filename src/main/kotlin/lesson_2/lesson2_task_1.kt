858f5b459c8fe6328336ba7814dee0f311c57741
artem <artemmrt@gmail.com>
2024-09-18 10:35:42 +0400

    KS-2-1: delete magic number, corrected the calculation formula

--- src/main/kotlin/lesson_2/lesson2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson2_task_1.kt
@@ -2,12 +2,10 @@ package org.example.lesson_2
 
 fun main() {
 
-    val scoreStudent1 = 3
-    val scoreStudent2 = 4
-    val scoreStudent3 = 3
-    val scoreStudent4 = 5
+    val scoreStudents = listOf(3, 4, 3, 5)
+    val quantityStudents = scoreStudents.size
 
-    val averageScore = (scoreStudent1.toDouble() + scoreStudent2.toDouble() + scoreStudent3.toDouble() + scoreStudent4.toDouble()) / 4
+    val averageScore = (scoreStudents.sum()).toDouble() / quantityStudents
 
     println(averageScore)
 
