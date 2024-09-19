37822d7f5434130d07d1ff14d7047c361f889295
artem <artemmrt@gmail.com>
2024-09-19 18:39:09 +0400

    KS-3-2 changed the data in the var

--- src/main/kotlin/lesson_3/lesson3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson3_task_2.kt
@@ -3,13 +3,14 @@ package org.example.lesson_3
 fun main() {
 
     val name = "Татьяна"
-    val surname = "Андреева"
+    var surname = "Андреева"
     val patronymic = "Сергеевна"
-    val surnameAfterMarriage = "Сидорова"
     val age = 20
     val weddingAge = 22
 
     println("$surname $name $patronymic, $age")
-    println("$surnameAfterMarriage $name $patronymic, $weddingAge")
+
+    surname = "Сидорова"
+    println("$surname $name $patronymic, $weddingAge")
 
 }
