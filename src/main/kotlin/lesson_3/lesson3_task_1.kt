7d90b35d1d39e979592b09a818841ff99769c20a
artem <artemmrt@gmail.com>
2024-09-19 11:35:31 +0400

    KS-3-1 do user greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val greetingDay = "Добрый день"
+    val greetingEvening = "Добрый вечер"
+    val userName = "Артем"
+
+    var greeting = println("$greetingDay, $userName!")
+    greeting = println("$greetingEvening, $userName!")
+
+}
