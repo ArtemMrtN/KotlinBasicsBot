a4280a1e06b31467cea9a28b2573adcea2926104
artem <artemmrt@gmail.com>
2024-09-19 11:49:32 +0400

    KS-3-2 personal data

---
+++ src/main/kotlin/lesson_3/lesson3_task_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    val surname = "Андреева"
+    val patronymic = "Сергеевна"
+    val surnameAfterMarriage = "Сидорова"
+    val age = 20
+    val weddingAge = 22
+
+    println("$surname $name $patronymic, $age")
+    println("$surnameAfterMarriage $name $patronymic, $weddingAge")
+
+}
