c6e28daebff8114ce7302ccd0e941bf1038b6d43
artem <artemmrt@gmail.com>
2024-09-28 11:58:03 +0400

    KS-6-5 Initial Commit

---
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -0,0 +1,51 @@
+package org.example.lesson_6
+
+fun main() {
+
+    println("Создайте свой аккаунт. \nВведите логин:")
+    val userLogin = readln()
+
+    println("Введите пароль:")
+    val userPassword = readln()
+
+    do {
+        println("Чтобы войти в приложение, введите свои данные. \nВведите логин:")
+        val userLoginAuth = readln()
+        println("Введите пароль:")
+        val userPasswordAuth = readln()
+    } while (userLoginAuth != userLogin || userPasswordAuth != userPassword)
+
+    var counter = 3
+
+    do {
+        val firstNumber = (1..9).random()
+        val secondNumber = (1..9).random()
+
+        println("Для входа в приложение решите простой математический пример: \n$firstNumber + $secondNumber = ?\nНапишите ответ:")
+
+        val exampleAnswer = firstNumber + secondNumber
+
+        val simpleMathExample = readln().toInt()
+
+        if (exampleAnswer == simpleMathExample) {
+            println("Добро пожаловать!")
+            break
+        } else {
+            counter--
+            println(counter)
+        }
+
+        if (counter == 0) {
+            println("Доступ запрещен.")
+        }
+
+    } while (counter > 0)
+
+}
+
+
+
+
+
+
+
