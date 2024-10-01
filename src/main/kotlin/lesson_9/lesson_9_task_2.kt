f1e44362e0ef42ed7ab998dd70147f5185b99a69
artem <artemmrt@gmail.com>
2024-10-01 18:09:17 +0400

    KS-9-2 Initial Commit

---
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_9
+
+fun main() {
+
+    val ingredients = mutableListOf("лук", "морковь", "картофель")
+
+    println("В рецепте есть базовые ингредиенты: $ingredients\nЖелаете добавить еще?")
+    val answer = readln()
+
+    if (answer == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val userIngredients = readln()
+        ingredients.add(userIngredients)
+        println("Теперь в рецепте есть следующие ингредиенты: $ingredients")
+    } else {
+        return
+    }
+
+
+
+}
