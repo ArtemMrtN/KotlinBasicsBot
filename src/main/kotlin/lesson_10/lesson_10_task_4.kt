e14d3c53018dc0644aeced4951304e2f268d9b35
artem <artemmrt@gmail.com>
2024-10-05 09:13:36 +0400

    KS-10-4 made a cycle "do...while", removed the global variable

--- src/main/kotlin/lesson_10/lesson_10_task_4.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -1,44 +1,45 @@
 package org.example.lesson_10
 
-var playerWin = 0
-
 fun main() {
 
-    println("Ход игрока:")
-    val playerMove = rollDice()
-    println(playerMove)
+    var playerWin = 0
 
-    println("Ход компьютера:")
-    val pcMove = rollDice()
-    println(pcMove)
+    do {
 
-    if (playerMove > pcMove) {
-        println("Победило человечество!")
-    } else if (playerMove < pcMove) {
-        println("Победила машина")
-    } else {
-        println("Ничья!")
-    }
+        println("Ход игрока:")
+        val playerMove = rollDice()
+        println(playerMove)
 
-    if (playerMove > pcMove) {
-        playerWin++
-    }
+        println("Ход компьютера:")
+        val pcMove = rollDice()
+        println(pcMove)
 
-    continueGame(playerWin)
+        if (playerMove > pcMove) {
+            println("Победило человечество!")
+        } else if (playerMove < pcMove) {
+            println("Победила машина")
+        } else {
+            println("Ничья!")
+        }
+
+        if (playerMove > pcMove) {
+            playerWin++
+        }
+
+    } while (startGame())
+
+    println("Количества выигрышных партий человека: $playerWin")
 
 }
 
 fun rollDice() = (1..6).random()
 
-fun continueGame(playerWin:Int) {
+fun startGame(): Boolean {
 
     println("Хотите бросить кости еще раз? Введите Да или Нет")
     val answer = readln()
 
-    if (answer == "Да" || answer == "да") {
-        main()
-    } else {
-        println("Количества выигрышных партий человека: $playerWin")
-    }
+    val resultAnswer = answer == "Да" || answer == "да"
 
+    return resultAnswer
 }
