d2b121dd2dde9389ce7979bb8eac773c389df9f3
artem <artemmrt@gmail.com>
2024-10-05 08:51:20 +0400

    KS-10-3 renamed the function, converted to String

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -5,22 +5,25 @@ fun main() {
     println("Задайте нужную длинну пароля:")
     val length = readln().toInt()
 
-    passwordLength(length)
+    println(generatePassword(length))
 
 }
 
-fun passwordLength(length: Int) {
+fun generatePassword(length: Int): String {
 
     val password: MutableList<String> = mutableListOf()
 
+    val randomSymbol = (32..47).map { it.toChar() }
+    val randomNumber = (0..9)
+
     for (i in 1..length) {
         val symbol = when {
-            i % 2 == 0 -> "!\"#\$%&'()*+,-./ ".toList().random()
-            else -> (0..9).random()
+            i % 2 == 0 -> randomSymbol.random()
+            else -> randomNumber.random()
         }
         password.add(symbol.toString())
     }
 
-    println(password.joinToString(""))
+    return password.joinToString("")
 
 }
