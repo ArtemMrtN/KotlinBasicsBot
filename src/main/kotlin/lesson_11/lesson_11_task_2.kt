9b8a4561529f5cd6eb41576fabb58a277a5d7ae1
artem <artemmrt@gmail.com>
2024-10-07 10:22:40 +0400

    KS-11-2 initial comment

---
+++ src/main/kotlin/lesson_11/lesson_11_task_2.kt
@@ -0,0 +1,50 @@
+package org.example.lesson_11
+
+class User2 (
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null,
+) {
+    fun userInformation () {
+        println(id)
+        println(login)
+        println(password)
+        println(email)
+        println(bio)
+
+        println("Заполните bio:")
+        bio = readln()
+
+        println("Вам необходимо сменить пароль. \nВведите текущий пароль:")
+        val enteredPassword = readln()
+        if (enteredPassword == password) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль успешно изменен!")
+        } else {
+            println("Вы ввели пароль неправильно.")
+        }
+    }
+}
+
+fun main() {
+
+    val user = User2 (
+        id = 1,
+        login = "Artem",
+        password = "1234",
+        email = "mail@gmail.com",
+    )
+
+    user.userInformation()
+
+    println("Обновленная информация:")
+    println(user.id)
+    println(user.login)
+    println(user.password)
+    println(user.email)
+    println(user.bio)
+
+}
