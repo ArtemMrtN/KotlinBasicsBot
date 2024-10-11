2f230c44af53f7782b9b946b30f7e4e1ad0da65c
artem <artemmrt@gmail.com>
2024-10-11 10:19:51 +0400

    KS-12-2 moved variables to constructor

--- src/main/kotlin/lesson_12/lesson_12_task_2.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_2.kt
@@ -1,13 +1,22 @@
 package org.example.lesson_12
 
-class WeatherForecast(_daytimeTemperature: Int, _nightTemperature: Int, _precipitation: Boolean) {
-
-    var daytimeTemperature = _daytimeTemperature
-    var nightTemperature = _nightTemperature
-    var precipitation = _precipitation
+class WeatherForecast(
+    var daytimeTemperature: Int,
+    var nightTemperature: Int,
+    var precipitation: Boolean
+) {
 
     fun checkWeather() {
-        println("Погода днем: $daytimeTemperature, погода ночью: $nightTemperature. Осадки: $precipitation")
+
+        val precipitationText: String
+
+        if (precipitation == false) {
+            precipitationText = "Нет"
+        } else {
+            precipitationText = "Да"
+        }
+
+        println("Погода днем: $daytimeTemperature, погода ночью: $nightTemperature. Осадки: $precipitationText")
     }
 
 }
