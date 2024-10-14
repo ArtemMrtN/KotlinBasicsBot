80c35d772aca2229e3de35d83cdc38625de48180
artem <artemmrt@gmail.com>
2024-10-14 11:04:21 +0400

    KS-13-1 initial comment

---
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_13
+
+class Contact(
+    val name: String,
+    val phoneNumber: Long,
+) {
+    var company: String? = null
+}
+
+fun main() {
+
+    val contact1 = Contact("Artem", 999L)
+    println(contact1.company)
+
+}
