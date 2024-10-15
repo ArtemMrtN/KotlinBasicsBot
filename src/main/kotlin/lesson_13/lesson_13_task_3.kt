767287b20f21a363d49f79216e43609f2153b154
artem <artemmrt@gmail.com>
2024-10-15 12:25:38 +0400

    KS-13-3 used mapNotNull and forEach

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -1,15 +1,14 @@
 package org.example.lesson_13
 
+val contactListAll: MutableList<ContactList> = mutableListOf()
+
 class ContactList(
     val name: String,
     val phoneNumber: Long,
     var company: String? = null
 ) {
-    val contactListAll: MutableList<ContactList> = mutableListOf()
-
     init {
         contactListAll.add(this)
-        println("Имя: ${contactListAll.map { it.name }.joinToString()} \nНомер: ${contactListAll.map { it.phoneNumber }.joinToString()}\nКомпания: ${contactListAll.map { it.company }.joinToString()}\n")
     }
 }
 
@@ -20,5 +19,7 @@ fun main() {
     val contact3 = ContactList("Ростислав", 89123456789, "null")
     val contact4 = ContactList("Владимир", 89123456789, "Google")
     val contact5 = ContactList("Александр", 89123456789, "Reddit")
+
+    contactListAll.mapNotNull { it.company }.forEach{println(it)}
 
 }
