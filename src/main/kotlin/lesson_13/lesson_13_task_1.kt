c04ae8f61c6f7f57e261b518e43b853cfaa1aa97
artem <artemmrt@gmail.com>
2024-10-16 15:53:07 +0400

    KS-13-1 wrote null when creating the object

--- src/main/kotlin/lesson_13/lesson_13_task_1.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -3,12 +3,12 @@ package org.example.lesson_13
 class Contact(
     val name: String,
     val phoneNumber: Long,
-    val company: String? = null
-) {}
+    val company: String?,
+)
 
 fun main() {
 
-    val contact1 = Contact("Artem", 999L)
+    val contact1 = Contact("Artem", 999L, null)
     println(contact1.company)
 
 }
