4dbfb67cbc9fb40f58deeff3a44bb2933ea99a53
artem <artemmrt@gmail.com>
2024-10-16 16:36:31 +0400

    KS-14-1 added parent

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -6,18 +6,11 @@ open class LinerShip(
     val numberOfPassengers: Int = 1000,
 )
 
-class CargoShip(
-    speed: Int = 10,
-    loadCapacity: Int = 50,
-    numberOfPassengers: Int = 5,
-)
+class CargoShip : LinerShip(10, 50, 5)
 
 class IcebreakerShip(
-    speed: Int = 10,
-    loadCapacity: Int = 20,
-    numberOfPassengers: Int = 5,
-    val ability: String = "Колоть лёд"
-)
+    val ability: String = "Колоть лёд",
+) : LinerShip(10, 20, 5)
 
 fun main() {
 
