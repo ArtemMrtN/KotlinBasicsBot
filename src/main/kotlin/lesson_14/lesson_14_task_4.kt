fb3fc392fb41a634e53cbf54106720fe820e5d53
artem <artemmrt@gmail.com>
2024-10-21 10:15:09 +0400

    KS-14-4 renamed to singular

--- src/main/kotlin/lesson_14/lesson_14_task_4.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_4.kt
@@ -1,32 +1,32 @@
 package org.example.lesson_14
 
-abstract class CelestialBodies(
+abstract class CelestialBody(
     val name: String,
     val atmosphere: Boolean,
     val suitableForPlanting: Boolean,
 )
 
-open class Planets(
+open class Planet(
     name: String,
     atmosphere: Boolean,
     suitableForPlanting: Boolean,
-    val allSatellites: MutableList<Satellites> = mutableListOf(),
-) : CelestialBodies(name, atmosphere, suitableForPlanting) {
-    fun assignParent(satellites: Satellites) {
+    val allSatellites: MutableList<Satellite> = mutableListOf(),
+) : CelestialBody(name, atmosphere, suitableForPlanting) {
+    fun assignParent(satellites: Satellite) {
         allSatellites.add(satellites)
     }
-    fun printSatellites(name: Planets) {
+    fun printSatellites(name: Planet) {
         println("${name.name}: ")
         name.allSatellites.forEach { println(it.name)}
     }
 }
 
-class Satellites(
+class Satellite(
     name: String,
     atmosphere: Boolean,
     suitableForPlanting: Boolean,
-    planet: Planets
-) : CelestialBodies(name, atmosphere, suitableForPlanting) {
+    planet: Planet
+) : CelestialBody(name, atmosphere, suitableForPlanting) {
     init {
         planet.assignParent(this)
     }
@@ -34,9 +34,9 @@ class Satellites(
 
 fun main() {
 
-    val planet1 = Planets("Планета 1", true, true)
-    val satellite1 = Satellites("Спутник 1", true, false, planet1)
-    val satellite2 = Satellites("Спутник 2", false, false, planet1)
+    val planet1 = Planet("Планета 1", true, true)
+    val satellite1 = Satellite("Спутник 1", true, false, planet1)
+    val satellite2 = Satellite("Спутник 2", false, false, planet1)
 
     planet1.printSatellites(planet1)
 
