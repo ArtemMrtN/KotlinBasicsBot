a6f4a5e8ff763c40bfa1aad05aa34e216dc0d0e9
artem <artemmrt@gmail.com>
2024-10-24 11:02:26 +0400

    KS-15-3 initial comment

---
+++ src/main/kotlin/lesson_15/lesson_15_task_3.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_15
+
+abstract class UserForum(
+    val name: String,
+) {
+    fun readForum() {
+        println("Читать форум")
+    }
+
+    fun writeMessage() {
+        println("Написать сообщение")
+    }
+}
+
+class RegularUserForum(name: String) : UserForum(name) {}
+
+class AdminUserForum(name: String) : UserForum(name) {
+    fun deleteMessage() {
+        println("Удалить сообщение")
+    }
+}
+
+fun main() {
+
+    val regularUser = RegularUserForum("Artem")
+    val adminUser = AdminUserForum("Ivan")
+
+    regularUser.readForum()
+    regularUser.writeMessage()
+    adminUser.deleteMessage()
+
+}
