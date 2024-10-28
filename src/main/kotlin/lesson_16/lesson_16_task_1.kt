27117029c2eb3b3c1da4bb4c0c8ea1547023abde
artem <artemmrt@gmail.com>
2024-10-28 10:38:15 +0400

    KS-16-1 initial comment

---
+++ src/main/kotlin/lesson_16/lesson_16_task_1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_16
+
+class Dice {
+    private val number = (1..6).random()
+
+    fun printNumber() {
+        println(number)
+    }
+}
+
+fun main() {
+
+    val dice = Dice()
+
+    dice.printNumber()
+
+}
