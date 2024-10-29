11de6fb7746242b9beb2a385b0393330263e4327
artem <artemmrt@gmail.com>
2024-10-29 11:59:55 +0400

    KS-15-5 moved the check to the class, added interface

--- src/main/kotlin/lesson_15/lesson_15_task_5.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_5.kt
@@ -1,73 +1,125 @@
 package org.example.lesson_15
 
-interface CargoMovement {
-    fun download(amountCargo: Int) {
-        println("Загрузили $amountCargo тонн груза")
+interface MovementCars {
+    fun come(name: String) {
+        println("Приехал $name")
     }
 
-    fun unload(amountCargo: Int) {
-        println("Разгрузили $amountCargo тонн груза")
+    fun leave(name: String) {
+        println("Уехал $name")
+    }
+
+    fun comeToEnd(name: String) {
+        println("$name приехал в точку назначения")
+    }
+}
+
+interface CargoMovement {
+    fun download(amountCargo: Int, name: String) {
+        println("Загрузили $amountCargo тонн груза в $name")
+    }
+
+    fun unload(amountCargo: Int, name: String) {
+        println("Разгрузили $amountCargo тонн груза из $name")
     }
 }
 
 interface PassengerMovement {
-    fun seatPassenger(amountPassenger: Int) {
-        println("Зашли $amountPassenger пассажира")
+    fun seatPassenger(amountPassenger: Int, name: String) {
+        println("Зашли $amountPassenger пассажира в $name")
     }
 
-    fun dropOffPassenger(amountPassenger: Int) {
-        println("Вышли $amountPassenger пассажира")
+    fun dropOffPassenger(amountPassenger: Int, name: String) {
+        println("Вышли $amountPassenger пассажира из $name")
     }
 }
 
 class Truck(
+    val name: String,
     val numberOfPassengers: Int,
     val weightOfCargo: Int,
-) : CargoMovement, PassengerMovement
+) : MovementCars, CargoMovement, PassengerMovement {
+    fun seatPassengers(fromTruck: Int) {
+        if (fromTruck > 0) {
+            seatPassenger(fromTruck, name)
+        }
+    }
+
+    fun dropOffPassengers(fromTruck: Int) {
+        if (fromTruck > 0) {
+            dropOffPassenger(fromTruck, name)
+        }
+    }
+
+    fun transferCargo(cargoToTransfer: Int) {
+        if (cargoToTransfer > 0) {
+            download(cargoToTransfer, name)
+        }
+    }
+
+    fun dropOffCargo(cargoToTransfer: Int) {
+        if (cargoToTransfer > 0) {
+            unload(cargoToTransfer, name)
+        }
+    }
+}
 
 class PassengerCar(
+    val name: String,
     val numberOfPassengers: Int,
-) : PassengerMovement
+) : MovementCars, PassengerMovement {
+    fun seatPassengers(fromCar: Int) {
+        if (fromCar > 0) {
+            seatPassenger(fromCar, name)
+        }
+    }
+
+    fun dropOffPassengers(fromCar: Int) {
+        if (fromCar > 0) {
+            dropOffPassenger(fromCar, name)
+        }
+    }
+}
 
 fun main() {
 
     var passengersNeedTransferred = 6
     var cargoNeedTransferred = 2
 
-    val passengerCar = PassengerCar(3)
-    val truck = Truck(1, 2)
+    val passengerCar = PassengerCar("Легковой автомобиль", 3)
+    val truck = Truck("Грузовик", 1, 2)
 
     do {
 
-        val passengersToSeat = minOf(passengersNeedTransferred, passengerCar.numberOfPassengers + truck.numberOfPassengers)
+        passengerCar.come(passengerCar.name)
+        truck.come(truck.name)
+
+        val passengersToSeat =
+            minOf(passengersNeedTransferred, passengerCar.numberOfPassengers + truck.numberOfPassengers)
         val fromCar = minOf(passengerCar.numberOfPassengers, passengersToSeat)
         val fromTruck = passengersToSeat - fromCar
 
-        if (fromCar > 0) {
-            passengerCar.seatPassenger(fromCar)
-            passengersNeedTransferred -= fromCar
-        }
-        if (fromTruck > 0) {
-            truck.seatPassenger(fromTruck)
-            passengersNeedTransferred -= fromTruck
-        }
+        passengerCar.seatPassengers(fromCar)
+        passengersNeedTransferred -= fromCar
+
+        truck.seatPassengers(fromTruck)
+        passengersNeedTransferred -= fromTruck
 
         val cargoToTransfer = minOf(cargoNeedTransferred, truck.weightOfCargo)
-        if (cargoToTransfer > 0) {
-            truck.download(cargoToTransfer)
-            cargoNeedTransferred -= cargoToTransfer
-        }
 
-        if (fromCar > 0) {
-            passengerCar.dropOffPassenger(fromCar)
-        }
-        if (fromTruck > 0) {
-            truck.dropOffPassenger(fromTruck)
-        }
+        truck.transferCargo(cargoToTransfer)
+        cargoNeedTransferred -= cargoToTransfer
 
-        if (cargoToTransfer > 0) {
-            truck.unload(cargoToTransfer)
-        }
+        passengerCar.leave(passengerCar.name)
+        truck.leave(truck.name)
+
+        passengerCar.comeToEnd(passengerCar.name)
+        truck.comeToEnd(truck.name)
+
+        passengerCar.dropOffPassengers(fromCar)
+        truck.dropOffPassengers(fromTruck)
+        truck.dropOffCargo(cargoToTransfer)
+
         println()
 
     } while (passengersNeedTransferred > 0 || cargoNeedTransferred > 0)
