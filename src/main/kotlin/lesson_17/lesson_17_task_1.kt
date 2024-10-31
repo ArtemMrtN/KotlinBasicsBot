efe3afcf022f7e9b400bbaa43f56ae945f62c40a
artem <artemmrt@gmail.com>
2024-10-31 15:02:07 +0400

    KS-17-1 initial comment

---
+++ src/main/kotlin/lesson_17/lesson_17_task_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_17
+
+class Quiz(
+    question: String,
+    answer: String,
+) {
+    val question = question
+        get() = field
+    var answer = answer
+        get() = field
+        set(value) {
+            field = value
+        }
+}
